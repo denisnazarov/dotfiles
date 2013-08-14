d3a8a655d43d0cf77a5279cb2363c6a0c2616add
Moudy <moudy.elkammash@gmail.com>
2013-08-14 09:58:13 -0400

    updates

--- zsh/install.sh
+++ zsh/install.sh
@@ -1,9 +1,3 @@
 if [ "$SHELL" != '/bin/zsh' ]; then chsh -s /bin/zsh; fi
 
-ANTIGEN_PATH=./zsh/zsh.symlink/antigen
-
-if [ -d "$ANTIGEN_PATH" ]; then
-  echo 'TODO: pull antigen latest'
-else
-  git submodule add https://github.com/zsh-users/antigen.git "$ANTIGEN_PATH"
-fi
+git submodule foreach git pull origin master
